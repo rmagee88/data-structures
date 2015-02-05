ac2f6fe4e6deddf8fee7e1ea2f98b4dfa0dfe73e
rmagee88 <rmagee88@gmail.com>
2015-02-05 10:31:35 -0800

    implemented linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,34 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  list.nodes = [];
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    var length = list.nodes.length;
+    if (length === 0) {
+      list.head = newNode;
+      list.tail = newNode;
+    }
+    list.nodes[length] = newNode;
+    list.tail.next = newNode;
+    list.tail = newNode;
+
   };
 
   list.removeHead = function(){
+    var oldHead = list.head;
+    var oldHeadValue = list.head.value;
+    list.head = list.head.next;
+    return list.nodes.shift().value;
+
   };
 
   list.contains = function(target){
+    for (var i = 0; i < list.nodes.length; i++) {
+      if (list.nodes[i].value === target) return true;
+    }
+    return false;
   };
 
   return list;
