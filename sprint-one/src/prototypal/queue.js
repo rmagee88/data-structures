7791383af2f88948322521efa9393ba63bb95de0
rmagee88 <rmagee88@gmail.com>
2015-02-04 16:21:18 -0800

    Finished implelementing Prototypal Stack and Queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.first = 0;
+  someInstance.last = 0;
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value) {
+    this.storage[this.last] = value;
+    this.last++;
+  },
+  dequeue : function() {
+    var result = this.storage[this.first];
+    delete this.storage[this.first];
+    this.first++;
+    return result;
+  },
+  size : function(){
+    return Math.max(0, this.last - this.first);
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.last = 0;
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push : function(value) {
+    this.storage[this.last] = value;
+    this.last++;
+  },
+  pop : function() {
+    var result = this.storage[this.last-1];
+    delete this.storage[this.last-1];
+    this.last && this.last--;
+    return result;
+  },
+  size : function() {
+    return this.last;
+  }
+};
 
 
