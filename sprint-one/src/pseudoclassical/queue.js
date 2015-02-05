b212a361e7035ab2bc4c6c174ad589b7d94d1517
rmagee88 <rmagee88@gmail.com>
2015-02-04 16:35:59 -0800

    implemented psuedoclassical queue & stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.first = 0;
+  this.last = 0;
+
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.last] = value;
+  this.last++;
+};
 
+Queue.prototype.dequeue = function() {
+  var result = this.storage[this.first];
+  delete this.storage[this.first];
+  this.first++;
+  return result;
+};
+
+Queue.prototype.size = function() {
+  return Math.max(0, this.last - this.first);
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.last = 0;
+  // this
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.last] = value;
+  this.last++;
+};
 
+Stack.prototype.pop = function() {
+  var result = this.storage[this.last-1];
+  delete this.storage[this.last-1];
+  this.last && this.last--;
+  return result;
+};
+
+Stack.prototype.size = function() {
+  return this.last;
+};
