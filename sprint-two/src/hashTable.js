ccdceb7debca5a286c1f6469cfda33b1d2647a77
rmagee88 <rmagee88@gmail.com>
2015-02-05 16:40:57 -0800

    Implemented hash table and set :D

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,16 +4,54 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+  console.log("k: " +k);
+  console.log("v: "+v)
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  if(this._storage.get(i) !== undefined){
+    var previous = this._storage.get(i);
+    if(!Array.isArray(previous)){
+      previous = [previous];
+    }
+    this._storage.set(i,previous.concat(v));
+  }else{
+  //   this._limit = this._limit*2;
+
+  //   var oldStorage = this._storage;
+  //   this._storage = LimitedArray(this._limit);
+
+  //   oldStorage.each(function(value, key, array){
+  //     this.insert(value, value);
+  //   });
+
+  //   this.insert(k,v);
+  //   // return   //redo index for imput
+  // }else{
+    this._storage.set(i, v);
+  }
+  // }
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var result = this._storage.get(i);
+  if(Array.isArray(result)){
 
+    for(var i = 0; i < result.length; i++){
+      if(result[i] === k){
+        return k;
+      }
+    }
+  }
+  // ["hello", "hi", "what", "adsfasd", "Adsfasdf "]
+  return this._storage.get(i);
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
 
+  this._storage.set(i, null);
 };
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(!this.contains(item)){
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item){
+  for(var i = 0; i < this._storage.length; i++){
+    if(this._storage[i] === item){
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  for(var i = 0; i < this._storage.length; i++){
+    if(this._storage[i] === item){
+      this._storage = this._storage.slice(0, i-1).concat(this._storage.slice(i+1));
+    }
+  }
 };
 
 /*
