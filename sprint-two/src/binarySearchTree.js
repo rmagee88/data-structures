4ec680a2e13c2b8e82986dc7baf1a869f8b647e5
rmagee88 <rmagee88@gmail.com>
2015-02-06 15:37:11 -0800

    Finished implementing the binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -60,13 +60,17 @@ BinarySearchTree.prototype.contains = function(value) {
 
 };
 
-BinarySearchTree.prototype.depthFirstLog = function() {
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+  callback(this.value)
 
+  if(this.left) this.left.depthFirstLog(callback);
+
+  if(this.right) this.right.depthFirstLog(callback);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
- insert -
- contains -
- depthFirstLog -
+ insert - O(log(n))
+ contains - O(log(n))
+ depthFirstLog - O(n)
  */
