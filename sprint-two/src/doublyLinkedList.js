299ca20ba9c2712c96c1ce4273bb966c6edcb447
rmagee88 <rmagee88@gmail.com>
2015-02-06 16:10:59 -0800

    created new files for doubly linked list

---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,64 @@
+var LinkedList = function(){
+  var list = {};
+  list.head = null;
+  list.tail = null;
+  list.nodes = [];
+
+  list.addToTail = function(value){
+    var newNode = Node(value);
+    var length = list.nodes.length;
+    if (length === 0) {
+      list.head = newNode;
+      list.tail = newNode;
+    }
+    newNode.previous = list.tail;
+    list.nodes[length] = newNode;
+    list.tail.next = newNode;
+    list.tail = newNode;
+
+  };
+
+  list.removeHead = function(){
+    var oldHead = list.head;
+    var oldHeadValue = list.head.value;
+    list.head = list.head.next;
+    return list.nodes.shift().value;
+
+  };
+
+  list.contains = function(target){
+    for (var i = 0; i < list.nodes.length; i++) {
+      if (list.nodes[i].value === target) return true;
+    }
+    return false;
+  };
+
+  list.addToHead = function(value){
+
+  }
+
+  list.removeTail = function(value){
+
+  }
+
+  return list;
+};
+
+var Node = function(value){
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+  node.previous = null;
+  return node;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+  addToTail - big O notation O(1)
+
+  removeHead - big O notation O(1)
+
+  contains - big O notation O(n)
+
+ */
